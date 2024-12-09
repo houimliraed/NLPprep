e358216c09ae77d26fbee3230eecde4fd979eaed
rayxtn <houimliraed@outlook.fr>
2024-12-10 00:37:11 +0100

    regex vs nltktokenizer

--- nlptraining.py
+++ nlptraining.py
@@ -1,1 +1,11 @@
 #importing needed libraries
+import nltk
+import regex
+
+text = "whatever u wouln't say is next with a space after this," ""
+# better word tokenizers example of use compared to regex
+regex_text = regex.split("[\s\.\,]",text)
+print(regex_text)
+print("***********************************************")
+nltk_text = nltk.word_tokenize(text)
+print(nltk_text)
